376f0a35670164efe7eec3dd67809944cc28320b
urbaniak <urbaniaknicolas62@gmail.com>
2017-11-22 18:25:30 +0100

    pdo

--- app/pdo.php
+++ app/pdo.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class pdo extends Model
 {
-    //
+    function connection(){
+        try {
+          $bdd= new PDO('mysql:host=localhost;dbname=exoThoma;metacharset=utf8','root','chniak24dec78-'
+          , array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+      }
+       catch (Exception $e)
+        {
+          die('erreur:'.$e->getMessage());
+      }
+       return $bdd;
+        }
 }
